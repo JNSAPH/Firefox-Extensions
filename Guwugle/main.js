21c9ba869142950a0cdb2983973f2843a1b1f932
Jonas <jnsaph@icloud.com>
2020-08-14 20:24:27 +0200

    Changed URL for Logo

--- Guwugle/main.js
+++ Guwugle/main.js
@@ -3,8 +3,8 @@ var domain = window.location.hostname.split(".")[1]
 if (domain == "google") {
     // HJandle Main Google Page
     // Change Logo
-    document.getElementById('hplogo').src = "https://cdn.jnsaph.com/Github/Guwugle/logo.png"
-    document.getElementById('hplogo').srcset = "https://cdn.jnsaph.com/Github/Guwugle/logo.png"
+    document.getElementById('hplogo').src = "https://raw.githubusercontent.com/JNSAPH/Firefox-Extensions/master/Guwugle/assets/logo.png"
+    document.getElementById('hplogo').srcset = "https://raw.githubusercontent.com/JNSAPH/Firefox-Extensions/master/Guwugle/assets/logo.png"
     document.getElementById('hplogo').style.width = "auto";
     document.getElementById('hplogo').height = 120;
 
